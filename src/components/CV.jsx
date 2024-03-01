d2591ea14eebaedc68e37b452e76989c43d1cc48
daniel-idowu-01 <danielidowu414@gmail.com>
2024-03-01 23:59:27 +0100

    linked cv from drive

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { styles } from '../style';
+import { Link } from 'react-router-dom';
 import { textVariant, slideIn } from '../utilities/motion';
 
 const CV = () => {
+
+  const btnStyle = "bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold uppercase tracking-wider shadow-md shadow-primary rounded-xl"
   return (
     <>
       <motion.div variants={textVariant()}>
@@ -11,9 +14,14 @@ const CV = () => {
         <h2 className={`${styles.sectionHeadText}`}>Get CV.</h2>
       </motion.div>
       <motion.div variants={slideIn("up", "spring", 0.3, 1)}>
-        <button className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold uppercase tracking-wider shadow-md shadow-primary rounded-xl">
-          Download Cv
-        </button>
+        <Link
+          to='https://drive.google.com/file/d/1-5J7KxNGZnRGcNZjvzBr6OC9lNHYEsOD/view?usp=drive_link'
+          target='_blank'
+        >
+          <button className={btnStyle}>
+            Download Cv
+          </button>
+        </Link>
       </motion.div>
     </>
   );
